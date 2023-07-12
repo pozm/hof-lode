3781e164b205ed8b3399a43ecca1f7b42b4e4689
aixeria <44528100+pozm@users.noreply.github.com>
2023-07-12 22:12:52 +0100

    better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,39 @@
+use chrono::{Local, NaiveDateTime, Utc};
+use sqlx::{query, query_as, FromRow, SqlitePool};
 use std::collections::HashMap;
 use std::time::SystemTime;
-use chrono::{Local, NaiveDateTime, Utc};
-use sqlx::{FromRow, query, query_as, SqlitePool};
 
 pub mod json;
-
 
 #[derive(FromRow, Debug, Clone)]
 #[sqlx(rename_all = "camelCase")]
 pub struct FCMem {
-    pub id : i64,
-    pub name : String,
-    pub rank : String,
-    pub avatar : String,
+    pub id: i64,
+    pub name: String,
+    pub rank: String,
+    pub avatar: String,
     #[sqlx(rename = "entryDate")]
-    pub entryDate : Option<chrono::NaiveDateTime>,
-    pub left : bool,
+    pub entryDate: Option<chrono::NaiveDateTime>,
+    pub left: bool,
     #[sqlx(rename = "leftDate")]
-    pub leftDate : Option<chrono::NaiveDateTime>
+    pub leftDate: Option<chrono::NaiveDateTime>,
 }
-
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    pub last_update : chrono::NaiveDateTime,
-    pub cache_data : Vec<FCMem>,
-    pub db : sqlx::SqlitePool
+    pub last_update: chrono::NaiveDateTime,
+    pub cache_data: Vec<FCMem>,
+    pub db: sqlx::SqlitePool,
 }
 
 impl AppState {
-    pub async  fn new() -> Self {
+    pub async fn new() -> Self {
         let db_con = sqlx::sqlite::SqlitePoolOptions::new()
             .connect("sqlite:./hof_lode.db?mode=rwc")
-            .await.unwrap();
-
-        sqlx::migrate!("./migrations")
-            .run(&db_con)
             .await
             .unwrap();
+
+        sqlx::migrate!("./migrations").run(&db_con).await.unwrap();
 
         let members = query_as!(FCMem, "SELECT * FROM fcMembers")
             .fetch_all(&db_con)
@@ -47,24 +43,29 @@ impl AppState {
         let last_update = query!("select last_update from updateTime where id = 1")
             .fetch_one(&db_con)
             .await
-            .unwrap().last_update.unwrap();
+            .unwrap()
+            .last_update
+            .unwrap();
 
         Self {
-            cache_data : members,
+            cache_data: members,
             last_update,
-            db:db_con
+            db: db_con,
         }
     }
 }
 
 pub async fn fetch_new_data() -> reqwest::Result<json::Root> {
-    let the_data = reqwest::get("https://xivapi.com/freecompany/9235053248388316377?data=FCM").await?.json::<json::Root>().await?;
+    let the_data = reqwest::get("https://xivapi.com/freecompany/9235053248388316377?data=FCM")
+        .await?
+        .json::<json::Root>()
+        .await?;
 
     Ok(the_data)
 }
-pub fn should_update(last : NaiveDateTime) -> bool {
+pub fn should_update(last: NaiveDateTime) -> bool {
     let now = Local::now().naive_local();
     let duration = now.signed_duration_since(last);
     let seconds = duration.num_seconds();
-    return seconds > (120*60)
+    return seconds > (120 * 60);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,140 @@
-
-use std::sync::Arc;
 use askama::Template;
-use axum::extract::{ State};
-use axum::{ Router};
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::get;
-use chrono::{Local};
-use sqlx::{Acquire, Executor, query, query_as,};
-use tower_http::services::ServeDir;
-use hof_lode::{AppState, FCMem, fetch_new_data, should_update};
+use axum::Router;
+use chrono::Local;
 use hof_lode::json::FreeCompanyMember;
+use hof_lode::{fetch_new_data, should_update, AppState, FCMem};
+use sqlx::{query, query_as, Acquire, Executor};
+use std::sync::Arc;
+use tower_http::services::ServeDir;
 
 type ApSt = Arc<tokio::sync::RwLock<AppState>>;
-
 
 #[derive(Template)]
 #[template(path = "index.askama", escape = "none")]
 struct IndexTemplate {
     new_members: Vec<FCMem>,
     gone_members: Vec<FCMem>,
-    members: Vec<FCMem>
+    members: Vec<FCMem>,
 }
-async fn the_the(State(data) : State<ApSt>) -> HtmlTemplate<IndexTemplate> {
+#[derive(Template)]
+#[template(path = "error.askama", escape = "none")]
+struct ErrorTemplate {
+    error: String,
+}
+#[derive(thiserror::Error, Debug)]
+enum TheErrors {
+    #[error("database error: {0}")]
+    DatabaseError(#[from] sqlx::Error),
+    #[error("fetch error: {0}")]
+    FetchError(#[from] reqwest::Error),
+}
 
-    let (last_update,oldm_st) = async {
-        let daa = data.read().await;
-        (daa.last_update.clone(),daa.cache_data.clone())
-    }.await;
+async fn the_the(State(data): State<ApSt>) -> Result<HtmlTemplate<impl Template>,HtmlTemplate<impl Template>> {
+    let templ : Result<IndexTemplate,TheErrors> = async {
+        let (last_update, oldm_st) = async {
+            let daa = data.read().await;
+            (daa.last_update.clone(), daa.cache_data.clone())
+        }.await;
 
-    let (mut data,new,gone) = if should_update(last_update) {
+        let (mut data, new, gone) = if should_update(last_update) {
+            println!("cache miss");
 
-        println!("cache miss");
+            let mut wd = data.write().await;
+            let new_data = fetch_new_data().await.map_err(TheErrors::FetchError)?;
+            println!("updating data -> {:?}", new_data.free_company.name);
 
-        let mut wd = data.write().await;
-        let new_data = fetch_new_data().await.unwrap();
-        println!("updating data -> {:?}", new_data.free_company.name);
+            let members_gone = oldm_st.iter().filter(|old| !new_data.free_company_members.iter().any(|new| new.id == old.id)).cloned().collect::<Vec<FCMem>>();
+            let new_members = new_data.free_company_members.iter().filter(|new| !oldm_st.iter().any(|old| new.id == old.id)).cloned().collect::<Vec<FreeCompanyMember>>();
 
-        let members_gone = oldm_st.iter().filter(|old| !new_data.free_company_members.iter().any(|new| new.id == old.id)).cloned().collect::<Vec<FCMem>>();
-        let new_members = new_data.free_company_members.iter().filter(|new| !oldm_st.iter().any(|old| new.id == old.id)).cloned().collect::<Vec<FreeCompanyMember>>();
+            let mut bulk_db = wd.db.begin().await.map_err(TheErrors::DatabaseError)?;
 
-        let mut bulk_db = wd.db.begin().await.unwrap();
+            for mem in &new_members {
+                println!("adding new member -> {:?}", mem.name);
+                sqlx::query!("INSERT OR IGNORE INTO fcMembers (id, name, rank,avatar,entryDate, left) VALUES (?,?,?,?,(SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now'))),0)",mem.id, mem.name, mem.rank,mem.avatar)
+                    .execute(&mut *bulk_db)
+                    .await
+                    .map_err(TheErrors::DatabaseError)?;
+            }
+            for mem in &members_gone {
+                println!("removing member -> {:?}", mem.name);
+                sqlx::query!("UPDATE fcMembers SET (left,leftDate) = (1,(SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now')))) WHERE id = ?", mem.id)
+                    .execute(&mut *bulk_db)
+                    .await
+                    .map_err(TheErrors::DatabaseError)?;
+            }
 
-        for mem in &new_members {
-            println!("adding new member -> {:?}", mem.name);
-            sqlx::query!("INSERT OR IGNORE INTO fcMembers (id, name, rank,avatar,entryDate, left) VALUES (?,?,?,?,(SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now'))),0)",mem.id, mem.name, mem.rank,mem.avatar)
+            query!("UPDATE updateTime SET last_update = (SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now'))) WHERE id = 1")
                 .execute(&mut *bulk_db)
                 .await
-                .unwrap();
-        }
-        for mem in &members_gone {
-            println!("removing member -> {:?}", mem.name);
-            sqlx::query!("UPDATE fcMembers SET (left,leftDate) = (1,(SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now')))) WHERE id = ?", mem.id)
-                .execute(&mut *bulk_db)
+                .map_err(TheErrors::DatabaseError)?;
+
+            bulk_db.commit().await.map_err(TheErrors::DatabaseError)?;
+
+            let members = query_as!(FCMem, "SELECT * FROM fcMembers")
+                .fetch_all(&wd.db)
                 .await
-                .unwrap();
-        }
+                .map_err(TheErrors::DatabaseError)?;
+            let new_members = if new_members.len() < 10 {
+                members.iter().filter(|new| !oldm_st.iter().any(|old| new.id == old.id)).cloned().collect::<Vec<FCMem>>()
+            } else {
+                Vec::new()
+            };
 
-        query!("UPDATE updateTime SET last_update = (SELECT strftime('%Y-%m-%d %H:%M:%S', datetime('now'))) WHERE id = 1")
-            .execute(&mut *bulk_db)
-            .await
-            .unwrap();
-
-        bulk_db.commit().await.unwrap();
-
-        let members = query_as!(FCMem, "SELECT * FROM fcMembers")
-            .fetch_all(&wd.db)
-            .await
-            .unwrap();
-        let new_members = if new_members.len() < 10 {
-            members.iter().filter(|new| !oldm_st.iter().any(|old| new.id == old.id)).cloned().collect::<Vec<FCMem>>()
+            wd.cache_data = members.clone();
+            wd.last_update = Local::now().naive_local();
+            (members, new_members, members_gone)
         } else {
-            Vec::new()
+            println!("cache hit");
+            (data.read().await.cache_data.clone(), Vec::new(), Vec::new())
         };
-
-        wd.cache_data = members.clone();
-        wd.last_update = Local::now().naive_local();
-        (members, new_members, members_gone)
-    } else {
-        println!("cache hit");
-        (data.read().await.cache_data.clone(), Vec::new(), Vec::new())
-    };
-    data.sort_by_key(|a| a.left);
-    let templt = IndexTemplate {
-        members: data,
-        new_members : new,
-        gone_members : gone
-    };
-    HtmlTemplate( templt )
+        data.sort_by_key(|a| a.left);
+        Result::<_,TheErrors>::Ok(IndexTemplate {
+            members: data,
+            new_members: new,
+            gone_members: gone
+        })
+    }.await;
+    return match templ {
+        Ok(t) => {
+            Ok(HtmlTemplate(t))
+        },
+        Err(e) => {
+            println!("error -> {:?}", e);
+            Err(HtmlTemplate(ErrorTemplate {
+                error: e.to_string(),
+            }))
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
-
-    let data =  Arc::new(tokio::sync::RwLock::new(AppState::new().await));
+    let data = Arc::new(tokio::sync::RwLock::new(AppState::new().await));
 
     let router = Router::new()
         .route("/", get(the_the))
         .nest_service("/assets", ServeDir::new("./assets/"))
         .with_state(data);
 
-
-    axum::Server::bind(&"0.0.0.0:3133".parse().unwrap()).serve(router.into_make_service()).await.unwrap();
-
-
+    axum::Server::bind(&"0.0.0.0:3133".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
 
 struct HtmlTemplate<T>(T);
 
 impl<T> IntoResponse for HtmlTemplate<T>
-    where
-        T: Template,
+where
+    T: Template,
 {
     fn into_response(self) -> Response {
         match self.0.render() {
-            Ok(html) => {
-                Html(html).into_response()
-            },
+            Ok(html) => Html(html).into_response(),
             Err(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 format!("Failed to render template. Error: {}", err),
